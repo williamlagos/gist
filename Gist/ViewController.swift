4db1885579b46e3fb00721a27ddb27eaf86a3dd2
William Oliveira de Lagos <william.lagos@icloud.com>
2021-04-18 02:32:47 +0100

    Added more controllers and a synced storyboard for the scanning

--- Gist/ViewController.swift
+++ Gist/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
-import AVFoundation
+import Foundation
+
+struct QRData {
+    var codeString: String?
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scannerView: ScannerView! {
+        didSet {
+            scannerView.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var scanButton: UIButton! {
+        didSet {
+            scanButton.setTitle("STOP", for: .normal)
+        }
+    }
+    
+    var qrData: QRData? = nil {
+        didSet {
+            if qrData != nil {
+                self.performSegue(withIdentifier: "detailSegue", sender: self)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !scannerView.isRunning {
+            scannerView.startScanning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if !scannerView.isRunning {
+            scannerView.stopScanning()
+        }
+    }
+    
+    @IBAction func scanButtonAction(_ sender: UIButton) {
+        scannerView.isRunning ? scannerView.stopScanning() : scannerView.startScanning()
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        sender.setTitle(buttonTitle, for: .normal)
+    }
+    
 }
 
+extension ViewController : ScannerViewDelegate {
+    
+    func scanningDidStop() {
+        let buttonTitle = scannerView.isRunning ? "STOP" : "SCAN"
+        scanButton.setTitle(buttonTitle, for: .normal)
+    }
+    
+    func scanningDidFail() {
+        
+    }
+    
+    func scanningSucceededWithCode(_ str: String?) {
+        self.qrData = QRData(codeString: str)
+    }
+    
+}
+
+extension ViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue", let viewController = segue.destination as? DetailViewController {
+            viewController.qrData = self.qrData
+        }
+    }
+    
+}

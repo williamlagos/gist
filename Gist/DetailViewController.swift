7abeeafe5c8aea86f3d8c794569a40a0b452cf1d
William Oliveira de Lagos <william.lagos@icloud.com>
2021-06-02 19:13:26 +0100

    Starting to evaluate tableview for displaying the comments

--- Gist/DetailViewController.swift
+++ Gist/DetailViewController.swift
@@ -10,7 +10,11 @@ import Foundation
 class DetailViewController : UIViewController {
     
     @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var comments: UITableView!
+    @IBOutlet weak var comment: UITextView!
+    @IBOutlet weak var accessToken: UITextField!
     var qrData: QRData?
+    var gistID: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +25,9 @@ class DetailViewController : UIViewController {
     }
     
     func retrieveGistComments(gistURL: String) {
-        print(gistURL.components(separatedBy: "/"))
         let decoupledURL = gistURL.components(separatedBy: "/")
-        let gistID = decoupledURL.last
-        let url = URL(string: "https://api.github.com/gists/\(gistID ?? "15370631fbb749e6db776f013a1ef8ad")/comments")!
+        self.gistID = decoupledURL.last
+        let url = URL(string: "https://api.github.com/gists/\(self.gistID ?? "15370631fbb749e6db776f013a1ef8ad")/comments")!
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 self.handleClientError(error: error)
@@ -42,6 +45,41 @@ class DetailViewController : UIViewController {
                 /*DispatchQueue.main.async {
                     self.webView.loadHTMLString(string, baseURL: url)
                 }*/
+            }
+        }
+        task.resume()
+    }
+    @IBAction func sendComment(_ sender: Any) {
+        self.postGistComments()
+    }
+    
+    func postGistComments() {
+        let url = URL(string: "https://api.github.com/gists/\(self.gistID ?? "15370631fbb749e6db776f013a1ef8ad")/comments")!
+        var request = URLRequest(url: url)
+        let parameters = ["body": comment.text ]
+        request.httpMethod = "POST"
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("Bearer \(accessToken.text ?? "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")", forHTTPHeaderField: "Authorization")
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                self.handleClientError(error: error)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                self.handleServerError(response: response!)
+                return
+            }
+            if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+                let data = data,
+                let string = String(data: data, encoding: .utf8) {
+                print(string)
             }
         }
         task.resume()
